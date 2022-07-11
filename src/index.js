245e8f18172c2452574d0312f69a7e07981a0faa
Dante4Rungun <denischuprin9619@gmail.com>
2022-07-11 19:53:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,27 +9,29 @@ const search = document.querySelector('.search')
 const moreBtn = document.querySelector('.load-more')
 moreBtn.setAttribute('hidden','')
 let searchString = ''
-let searchStringSubmit =''
+let searchStringSubmit = ''
 let page =  1
 
 search.addEventListener("input", (event) => {
     searchString = event.target.value
 })
 
-async function galleryAxiosCreate(searchString,page,galleryLength){
+async function galleryAxiosCreate(searchString,page){
 
         moreBtn.setAttribute('hidden','')
         try{
-            const gallery = await axiosGalerry(searchString,page)
-            if(galleryLength <= page*40){
+            const galleryItems = await axiosGalerry(searchString,page)
+            if(galleryItems.totalHits <= page*40){
                 Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")
                 moreBtn.setAttribute('hidden','')
             }
-            else if(gallery.hits.length === 0){
+            else if(galleryItems.hits.length === 0){
+                gallery.innerHTML = ""
                 Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
             }
             else{
-                createGallery(gallery)
+                gallery.innerHTML = ""
+                createGallery(galleryItems)
                 moreBtn.removeAttribute('hidden')
             }
 
